a29c45f448f77e22c1bed1cc8852799079c17cef
oserris <olivier@isir.upmc.fr>
2022-04-28 14:29:41 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["jax>=0.3.0", "jaxlib>=0.3.0", "brax", "chex", "numpy",
+                "seaborn", "matplotlib", "tqdm", "pandas", "omegaconf",
+                "hydra-core", ]
+setup(
+    name="jax benchmark",
+    version="0.0.1",
+    author="Olivier Serris",
+    author_email="serris@isir.upmc.fr",
+    description="Jax benchmark for rollouts  ",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olivier-serris/jax_benchmark",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    entry_points={"console_scripts": ["hydra_app = hydra_app.main:main"]},
+    python_requires=">=3.7",
+)
